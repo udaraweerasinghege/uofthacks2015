e6422157576655daf3a971e496e137fd7a72399c
Pavitheran Subenderan <psubende@uwaterloo.ca>
2015-01-31 12:07:30 -0500

    Added parameter validation

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,7 +1,7 @@
 function AppCtrl($scope, $http){
     console.log("Hello World");
-
-/*$http.get('/public/contactlist').success(function(response) {
+/*
+$http.get('/public/contactlist').success(function(response) {
     console.log('I got the data I requested');
     console.log(response);
 });*/
--- server.js
+++ server.js
@@ -7,21 +7,37 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-/*
+
 app.get('/public/contactlist', function(req, res){
     console.log("got get req");
     db.contactlist.find(function(err, docs){
        console.log(docs);
        res.json(docs);
     });
-});*/
+});
 
 app.post('/public/contactlist', function(req, res) {
     console.log(req.body);
-    db.contactlist.insert(req.body, function(err, doc){
-        res.json(doc);
-    })
+    count = Object.size(req.body);
+    
+    if (count == 4) {
+        console.log(db.contactlist.find({email: {"$in": req.body.email}}).count());
+        db.contactlist.insert(req.body, function(err, doc){
+            res.json(doc);
+    
+        });
+    }
+    
+    else {
+        console.log("Invalid parameters, db not updated");
+    }
 });
-
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
 app.listen(3000);
 console.log("Server running on port 3000");
