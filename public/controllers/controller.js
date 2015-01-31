a8b9b7d32151c0692e32b9e929101f9f0925bfd5
Pavitheran Subenderan <psubende@uwaterloo.ca>
2015-01-31 01:55:42 -0500

    Added nodejs/mongodb

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,3 +1,16 @@
-function AppCtrl(){
-    console.log("sup");
+function AppCtrl($scope, $http){
+    console.log("Hello World");
+
+/*$http.get('/public/contactlist').success(function(response) {
+    console.log('I got the data I requested');
+    console.log(response);
+});*/
+
+$scope.addContact = function() {
+    //console.log($scope.contact);
+    $http.post('/public/contactlist', $scope.contact).success(function(response){
+        console.log(response);
+    });
+};
+        
 }
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+/*
+app.get('/public/contactlist', function(req, res){
+    console.log("got get req");
+    db.contactlist.find(function(err, docs){
+       console.log(docs);
+       res.json(docs);
+    });
+});*/
 
-
-
-// assuming POST: name=foo&color=red            <-- URL encoding
-//
-// or       POST: {"name":"foo","color":"red"}  <-- JSON encoding
-
-app.post('/test-page', function(req, res) {
-    var name = req.body.name,
-        color = req.body.color;
-    // ...
+app.post('/public/contactlist', function(req, res) {
+    console.log(req.body);
+    db.contactlist.insert(req.body, function(err, doc){
+        res.json(doc);
+    })
 });
 
 app.listen(3000);
