83c1625929c7468e3918a09f78eeabbfc486a556
chen_solomon <richardb.chen@utoronto.ca>
2015-02-01 00:42:50 -0500

    More cosmetic changes

--- public/controllers/backgroundVideo.js
+++ public/controllers/backgroundVideo.js
@@ -2,6 +2,5 @@ jQuery.noConflict();
 jQuery(document).ready(function($) {
     var BV = new $.BigVideo();
     BV.init();
-    BV.show('http://vjs.zencdn.net/v/oceans.mp4');
+    BV.show('video/AmITopOrBottom.mp4');
 });
-
