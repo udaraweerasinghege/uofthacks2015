4ba01fc194d873320c1735ee74e1956345611827
udaraweerasinghege <udara.weerasinghege@mail.utoronto.ca>
2015-02-01 21:36:53 -0500

    cool new alerts

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -23,21 +23,21 @@ $http.get('/public/contactlist').success(function(response) {
 	};
 	
 	$scope.addContact = function() {
-            alert("Please be patient while we verify your email. May take upto 30 seconds.\nPress Ok");
+            sweetAlert("Please be patient while we verify your email.", "May take upto 30 seconds.\n              Press Ok");
 		console.log($scope.contact);
 		$http.post('/public/contactlist', $scope.contact).success(function(response){
 	   
 			if (response === "0") {
-				alert("This email has been used before to send candy. Please contact us for changes (6472982364)");
+				swal("This email has been used before to send Candy.", "Please contact us for changes to the CandyGram or to request support at (+1 647 298 2364)", "error");
 			}
 			
 			else if  (response === "1") {
-				alert("Valid UofT Email Addresses only ");
+				swal("You entered an invalid or non-existing E-mail. Please enter a valid UofT email", "If you think we're mistaken contact us at (+1 647 298 2364)", "error");
 			}
 			
 			else {
 				console.log(response);
-				alert("Success! Candygram will be delivered soon!");
+				swal("Success!Your CandyGram will be delivered soon!", "success");
 				
 			}
 			
